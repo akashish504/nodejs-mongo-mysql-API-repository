9c78e4efc876f1ca7a93f28934e803fef410f05a
Ashish <apple@Apples-MacBook-Pro.local>
2024-01-17 21:25:32 +0530

    first commit

---
+++ src/db_connections/database.js
@@ -0,0 +1,38 @@
+const mysqlDriver = require('mysql2');
+const mongoose = require('mongoose');
+
+const sqlDBconnection = mysqlDriver.createConnection({
+    host     : 'host.docker.internal',
+    user     : 'root',
+    password : 'qwerty123',
+    database : 'finkraft_user_db'
+});
+
+const nosqlDBconnecttion = () => {
+    const url = "mongodb://host.docker.internal:27017/product_info";
+   
+    try {
+        mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+    const dbConnection = mongoose.connection;
+    
+    dbConnection.once("open", (_) => {
+        console.log(`Database connected: ${url}`);
+    });
+   
+    dbConnection.on("error", (err) => {
+        console.error(`connection error: ${err}`);
+    });
+    return;
+}
+
+module.exports = {
+    sqlDBconnection,
+    nosqlDBconnecttion
+}
